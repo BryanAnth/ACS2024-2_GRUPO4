ecbebfd09670b747f882a5de5dedc1aec2bcfe3d
SebastianCFlores <sebastian.cespedes1@unmsm.edu.pe>
2024-11-27 10:35:34 -0500

    agregando try except para validar si no se encuentra el archivo, que se inicialice un diccionario vacio

--- utils/storage.py
+++ utils/storage.py
@@ -7,3 +7,11 @@ class ValueFunctionStorage:
         with open(self.filename, 'wb') as f:
             pickle.dump(agent.value_function, f)
         print(f"Value function saved to {self.filename}")
+    def load(self,agent):
+        try:
+            with open(self.filename, 'rb') as f:
+                agent.value_function = pickle.load(f)
+            print(f"Value function loaded from {self.filename}")
+        except FileNotFoundError:
+            print(f"No value function found at {self.filename}")
+            agent.value_function = {}
