655a59c1d59c0b3274ff41509693ade7308cd932
SebastianCFlores <sebastian.cespedes1@unmsm.edu.pe>
2024-11-27 10:26:44 -0500

    definir la funcion de guardado en un archivo

--- utils/storage.py
+++ utils/storage.py
@@ -3,3 +3,6 @@ import pickle
 class ValueFunctionStorage:
     def __init__(self, filename="value_function.pkl"):
         self.filename = filename
+    def save(self,agent):
+        with open(self.filename, 'wb') as f:
+            pickle.dump(agent.value_function, f)
