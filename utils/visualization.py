8129a7ce8d839aaa16bc73126e902729884a9b14
SebastianCFlores <sebastian.cespedes1@unmsm.edu.pe>
2024-11-27 11:44:35 -0500

    agregando visualization manager class, inicializando el paquete[

--- utils/visualization.py
+++ utils/visualization.py
@@ -3,3 +3,8 @@ import numpy as np
 import seaborn as sns
 from typing import List, Dict, Tuple
 import ipywidgets as widgets
+
+class VisualizationManager:
+    def __init__(self):
+        sns.set_theme()
+        self.default_figsize = (12, 6)
