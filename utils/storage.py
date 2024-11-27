40b6702cdd29818ec6b861a0fa6aa200dc2ddac5
SebastianCFlores <sebastian.cespedes1@unmsm.edu.pe>
2024-11-27 10:36:34 -0500

    agregando comentarios de codigo

--- utils/storage.py
+++ utils/storage.py
@@ -4,14 +4,21 @@ class ValueFunctionStorage:
     def __init__(self, filename="value_function.pkl"):
         self.filename = filename
     def save(self,agent):
+        """
+        Guarda el value_function del agente en un archivo.
+        """
         with open(self.filename, 'wb') as f:
             pickle.dump(agent.value_function, f)
         print(f"Value function saved to {self.filename}")
     def load(self,agent):
+        """
+        Carga el value_function del agente desde un archivo.
+        Si el archivo no existe, inicializa un diccionario vacío.
+        """
         try:
             with open(self.filename, 'rb') as f:
                 agent.value_function = pickle.load(f)
             print(f"Value function loaded from {self.filename}")
         except FileNotFoundError:
             print(f"No value function found at {self.filename}")
-            agent.value_function = {}
+            agent.value_function = {} # Inicializa un diccionario vacío si no se encuentra el archivo
